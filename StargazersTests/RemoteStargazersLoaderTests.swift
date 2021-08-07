463c8cc963ff30220f2ac837db027d313fca9903
Riccardo Rossi <riki.redz@gmail.com>
2021-08-08 01:56:52 +0200

    `RemoteStargazersLoader`'s `load` command does request data from URL.

--- StargazersTests/RemoteStargazersLoaderTests.swift
+++ StargazersTests/RemoteStargazersLoaderTests.swift
@@ -8,18 +8,34 @@
 import XCTest
 
 final class RemoteStargazersLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = [URL(string: "http://a-url.com")!]
+    }
 }
 
 class HTTPClient {
+    private init() {}
+    
+    static let shared = HTTPClient()
+    
     var requestedURL = [URL]()
 }
 
 class RemoteStargazersLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteStargazersLoader()
         
         XCTAssertEqual(client.requestedURL, [])
+    }
+    
+    func test_load_doesRequestDataFromURL() {
+        let client = HTTPClient.shared
+        let url = URL(string: "http://a-url.com")!
+        let sut = RemoteStargazersLoader()
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, [url])
     }
 }
