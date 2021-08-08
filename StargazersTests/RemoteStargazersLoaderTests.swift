9e5b96e437cc974e5abf0c0a0673d3fa2a1c3545
Riccardo Rossi <riki.redz@gmail.com>
2021-08-08 15:26:01 +0200

    `load` command delivers invalid data error on client error.

--- StargazersTests/RemoteStargazersLoaderTests.swift
+++ StargazersTests/RemoteStargazersLoaderTests.swift
@@ -6,23 +6,30 @@
 //
 
 import XCTest
+import Stargazers
 
 final class RemoteStargazersLoader {
     private let client: HTTPClient
     private let url: URL
+    
+    enum Error: Swift.Error {
+        case invalidData
+    }
     
     init(client: HTTPClient, url: URL) {
         self.client = client
         self.url = url
     }
     
-    func load() {
-        client.get(from: url)
+    func load(completion: @escaping (Swift.Error) -> Void) {
+        client.get(from: url) { _ in
+            completion(Error.invalidData)
+        }
     }
 }
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 class RemoteStargazersLoaderTests: XCTestCase {
@@ -36,9 +43,23 @@ class RemoteStargazersLoaderTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let (sut, client) = makeSUT(for: url)
         
-        sut.load()
+        sut.load() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_load_deliversInvalidDataErrorOnClientError() {
+        let (sut, client) = makeSUT(for: URL(string: "http://any-url.com")!)
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { error in
+            XCTAssertEqual(error as? RemoteStargazersLoader.Error, .invalidData)
+            exp.fulfill()
+        }
+        
+        client.complete(with: NSError(domain: "any", code: -1))
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func makeSUT(for url: URL = URL(string: "http://a-default-url.com")!) -> (RemoteStargazersLoader, HTTPClientSpy) {
@@ -49,9 +70,15 @@ class RemoteStargazersLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var completions = [(Error) -> Void]()
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
             requestedURLs.append(url)
+            completions.append(completion)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
