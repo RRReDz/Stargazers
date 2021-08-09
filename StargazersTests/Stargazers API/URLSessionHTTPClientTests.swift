47bf6184fe2fe8bc46d447d4dfc9babdd6f0037b
Riccardo Rossi <riki.redz@gmail.com>
2021-08-09 16:57:26 +0200

    `URLSessionHTTPClient`'s `get` command does request data from URL.

--- StargazersTests/Stargazers API/URLSessionHTTPClientTests.swift
+++ StargazersTests/Stargazers API/URLSessionHTTPClientTests.swift
@@ -8,15 +8,29 @@
 import XCTest
 
 final class URLSessionHTTPClient {
-    private let session: URLSessionSpy
+    private let session: URLSession
     
-    init(session: URLSessionSpy) {
+    init(session: URLSession) {
         self.session = session
+    }
+    
+    func get(from url: URL) {
+        _ = session.dataTask(with: url)
     }
 }
 
-final class URLSessionSpy {
+final class URLSessionSpy: URLSession {
     var requestedURLs = [URL]()
+    
+    override init() {}
+    
+    override func dataTask(with url: URL) -> URLSessionDataTask {
+        class AnyTask: URLSessionDataTask {}
+        
+        requestedURLs.append(url)
+        
+        return AnyTask()
+    }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
@@ -26,6 +40,34 @@ class URLSessionHTTPClientTests: XCTestCase {
         _ = URLSessionHTTPClient(session: session)
         
         XCTAssertEqual(session.requestedURLs, [])
+    }
+    
+    func test_get_doesRequestDataFromURL() {
+        let url = anyURL()
+        let (sut, session) = makeSUT()
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.requestedURLs, [url])
+    }
+    
+    //MARK: - Utils
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (URLSessionHTTPClient, URLSessionSpy) {
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        trackForMemoryLeak(sut, file: file, line: line)
+        trackForMemoryLeak(session, file: file, line: line)
+        return (sut, session)
+    }
+    
+    private func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
     }
 
 }
