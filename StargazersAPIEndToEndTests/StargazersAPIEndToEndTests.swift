4b2b169a0e0d98fb1ef670ad67faf8fe66dc778b
Riccardo Rossi <riki.redz@gmail.com>
2021-08-12 00:12:57 +0200

    Adds `StargazersAPIEndToEndTests` target and scheme.

---
+++ StargazersAPIEndToEndTests/StargazersAPIEndToEndTests.swift
@@ -0,0 +1,10 @@
+//
+//  StargazersAPIEndToEndTests.swift
+//  StargazersAPIEndToEndTests
+//
+//  Created by Riccardo Rossi - Home on 12/08/21.
+//
+
+import XCTest
+
+class StargazersAPIEndToEndTests: XCTestCase {}
