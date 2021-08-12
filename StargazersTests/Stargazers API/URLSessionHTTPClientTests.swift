dc2d31f17874c170435886a9e3b886899e51ca8d
Riccardo Rossi <riki.redz@gmail.com>
2021-08-12 19:40:41 +0200

    Increases wait for expectation timeout time for specific test to avoid unexpected failures.

--- StargazersTests/Stargazers API/URLSessionHTTPClientTests.swift
+++ StargazersTests/Stargazers API/URLSessionHTTPClientTests.swift
@@ -125,7 +125,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
 
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 2.0)
         
         return capturedResult
     }
