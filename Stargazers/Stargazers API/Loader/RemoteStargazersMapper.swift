20ed7faca1fac0853d06f0049920329d6dd8678e
Riccardo <riki.redz@gmail.com>
2021-09-02 22:43:29 +0200

    Reorders code to be compliant to codacy linting rules.

--- Stargazers/Stargazers API/Loader/RemoteStargazersMapper.swift
+++ Stargazers/Stargazers API/Loader/RemoteStargazersMapper.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 internal final class RemoteStargazersMapper {
-    
     internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [RemoteStargazer] {
         guard
             response.isOK,
