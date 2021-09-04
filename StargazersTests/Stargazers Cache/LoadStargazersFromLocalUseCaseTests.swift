ba3d828fe174e027ea8d135c2bc6150584c282e4
Riccardo <riki.redz@gmail.com>
2021-09-04 17:11:09 +0200

    Creates `CacheStargazersUseCaseTests` with all the relative command implementations and helpers.

--- StargazersTests/Stargazers Cache/LoadStargazersFromLocalUseCaseTests.swift
+++ StargazersTests/Stargazers Cache/LoadStargazersFromLocalUseCaseTests.swift
@@ -53,140 +53,6 @@ class LoadStargazersFromLocalUseCaseTests: XCTestCase {
         })
     }
     
-    // MARK: - Save Stargazers
-    
-    func test_save_sendStoreDeleteRepositoryStargazersMessage() {
-        let (sut, store) = makeSUT()
-        let repository = useCaseRepository()
-        
-        sut.save(uniqueStargazers().model, for: repository.model) { _ in }
-        
-        XCTAssertEqual(store.messages, [.deleteStargazers(for: repository.local)])
-    }
-    
-    func test_save_sendOnlyDeleteMessageAfterDeletionError() {
-        let (sut, store) = makeSUT()
-        let repository = useCaseRepository()
-        
-        sut.save(uniqueStargazers().model, for: repository.model) { _ in }
-        store.completeDeletionWithError(anyNSError())
-        
-        XCTAssertEqual(store.messages, [.deleteStargazers(for: repository.local)])
-    }
-    
-    func test_save_sendStoreDeleteAndInsertMessagesOnDeletionSuccess() {
-        let (sut, store) = makeSUT()
-        let repository = useCaseRepository()
-        let stargazers = uniqueStargazers()
-        
-        sut.save(stargazers.model, for: repository.model) { _ in }
-        store.completeDeletionSuccessfully()
-        
-        XCTAssertEqual(store.messages, [
-            .deleteStargazers(for: repository.local),
-            .insert(stargazers.local, for: repository.local)
-        ])
-    }
-    
-    func test_save_sendStoreDeleteAndInsertMessagesOnDeletionSuccessAndInsertionError() {
-        let (sut, store) = makeSUT()
-        let repository = useCaseRepository()
-        let stargazers = uniqueStargazers()
-        
-        sut.save(stargazers.model, for: repository.model) { _ in }
-        store.completeDeletionSuccessfully()
-        store.completeInsertionWithError(anyNSError())
-        
-        XCTAssertEqual(store.messages, [
-            .deleteStargazers(for: repository.local),
-            .insert(stargazers.local, for: repository.local)
-        ])
-    }
-    
-    func test_save_sendStoreDeleteAndInsertMessagesOnDeletionSuccessAndInsertionSuccess() {
-        let (sut, store) = makeSUT()
-        let repository = useCaseRepository()
-        let stargazers = uniqueStargazers()
-        
-        sut.save(stargazers.model, for: repository.model) { _ in }
-        store.completeDeletionSuccessfully()
-        store.completeInsertionSuccessfully()
-        
-        XCTAssertEqual(store.messages, [
-            .deleteStargazers(for: repository.local),
-            .insert(stargazers.local, for: repository.local)
-        ])
-    }
-    
-    func test_save_deliversErrorOnDeletionError() {
-        let (sut, store) = makeSUT()
-        let error = anyNSError()
-        
-        assert(that: sut, completesSaveWith: .failure(error), on: {
-            store.completeDeletionWithError(error)
-        })
-    }
-    
-    func test_save_doesNotDeliverResultJustOnDeletionSuccess() {
-        let (sut, store) = makeSUT()
-        
-        assert(sut, saveDoesNotDeliverResultsOn: {
-            store.completeDeletionSuccessfully()
-        })
-    }
-    
-    func test_save_deliversErrorOnDeletionSuccessAndInsertionError() {
-        let (sut, store) = makeSUT()
-        let error = anyNSError()
-        
-        assert(that: sut, completesSaveWith: .failure(error), on: {
-            store.completeDeletionSuccessfully()
-            store.completeInsertionWithError(error)
-        })
-    }
-    
-    func test_save_deliversSuccessOnDeletionSuccessAndInsertionSuccess() {
-        let (sut, store) = makeSUT()
-        
-        assert(that: sut, completesSaveWith: .success(()), on: {
-            store.completeDeletionSuccessfully()
-            store.completeInsertionSuccessfully()
-        })
-    }
-    
-    func test_save_doesNotSendStoreInsertMessageWhenInstanceHasBeenDeallocatedAndCompleteDeletionSuccessfully() {
-        let stargazers = uniqueStargazers().model
-        let repository = useCaseRepository()
-        var (sut, store) = makeOptionalSUT()
-        
-        sut?.save(stargazers, for: repository.model) { _ in }
-        
-        sut = nil
-        
-        store.completeDeletionSuccessfully()
-        
-        XCTAssertEqual(store.messages, [.deleteStargazers(for: repository.local)])
-    }
-    
-    func test_save_doesNotDeliverResultWhenInstanceHasBeenDeallocatedAndCompleteDeletionWithError() {
-        var (sut, store) = makeOptionalSUT()
-        
-        assert(sut.toWeak, saveDoesNotDeliverResultsOn: {
-            sut = nil
-            store.completeDeletionWithError(anyNSError())
-        })
-    }
-    
-    func test_save_doesNotDeliverResultOnSuccessfulDeletionThenInstanceIsDeallocatedAndInsertionCompleted() {
-        var (sut, store) = makeOptionalSUT()
-        
-        assert(sut.toWeak, saveDoesNotDeliverResultsOn: {
-            store.completeDeletionSuccessfully()
-            sut = nil
-            store.completeInsertionSuccessfully()
-        })
-    }
-    
     // MARK: - Load Stargazers
     
     func test_load_sendStoreRetrieveRepositoryMessage() {
@@ -341,44 +207,6 @@ class LoadStargazersFromLocalUseCaseTests: XCTestCase {
     }
     
     private func assert(
-        that sut: LocalStargazersLoader,
-        completesSaveWith expectedResult: Result<Void, Error>,
-        on action: () -> Void,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        let exp = expectation(description: "Wait for save completion")
-        sut.save(uniqueStargazers().model, for: anyRepository()) { receivedResult in
-            switch (expectedResult, receivedResult) {
-            case (.success, .success):
-                break
-            case let (.failure(expectedError as NSError), .failure(receivedError as NSError)):
-                XCTAssertEqual(expectedError, receivedError, file: file, line: line)
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func assert(
-        _ sut: StargazersSaver,
-        saveDoesNotDeliverResultsOn action: () -> Void,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        let stargazers = uniqueStargazers().model
-        let repository = useCaseRepository()
-        var capturedResults = [Any]()
-        sut.save(stargazers, for: repository.model) { capturedResults.append($0) }
-        assertIsEmpty(capturedResults, on: action, file: file, line: line)
-    }
-    
-    private func assert(
         _ sut: StargazersLoader,
         loadDoesNotDeliverResultsOn action: () -> Void,
         file: StaticString = #filePath,
@@ -406,8 +234,8 @@ class LoadStargazersFromLocalUseCaseTests: XCTestCase {
         _ items: [Any],
         on action: () -> Void,
         file: StaticString = #filePath,
-        line: UInt = #line)
-    {
+        line: UInt = #line
+    ) {
         action()
         XCTAssert(items.isEmpty, "Expected no items, got \(items) instead.", file: file, line: line)
     }
