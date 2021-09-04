0f2b9266cd37aa641b7c171b206c2c26992791fc
Riccardo <riki.redz@gmail.com>
2021-09-04 17:31:58 +0200

    Moves `StargazersStoreSpy` to its own file allowing remove duplicate code.

--- StargazersTests/Stargazers Cache/CacheStargazersUseCaseTests.swift
+++ StargazersTests/Stargazers Cache/CacheStargazersUseCaseTests.swift
@@ -210,58 +210,6 @@ class CacheStargazersUseCaseTests: XCTestCase {
         XCTAssert(items.isEmpty, "Expected no items, got \(items) instead.", file: file, line: line)
     }
     
-    private class StargazersStoreSpy: StargazersStore {
-        enum Message: Equatable {
-            case retrieveStargazers(for: LocalRepository)
-            case deleteStargazers(for: LocalRepository)
-            case insert([LocalStargazer], for: LocalRepository)
-        }
-        
-        private(set) var messages = [Message]()
-        private var retrieveCompletions = [RetrieveCompletion]()
-        private var deleteCompletions = [DeleteCompletion]()
-        private var insertionCompletions = [InsertCompletion]()
-        
-        func retrieve(from repository: LocalRepository, completion: @escaping RetrieveCompletion) {
-            messages.append(.retrieveStargazers(for: repository))
-            retrieveCompletions.append(completion)
-        }
-        
-        func insert(_ stargazers: [LocalStargazer], for repository: LocalRepository, completion: @escaping InsertCompletion) {
-            messages.append(.insert(stargazers, for: repository))
-            insertionCompletions.append(completion)
-        }
-        
-        func deleteStargazers(for repository: LocalRepository, completion: @escaping DeleteCompletion) {
-            messages.append(.deleteStargazers(for: repository))
-            deleteCompletions.append(completion)
-        }
-        
-        func completeRetrievalWithError(_ error: Error, at index: Int = 0) {
-            retrieveCompletions[index](.failure(error))
-        }
-        
-        func completeRetrievalSuccessfully(with stargazers: [LocalStargazer], at index: Int = 0) {
-            retrieveCompletions[index](.success(stargazers))
-        }
-        
-        func completeDeletionWithError(_ error: Error, at index: Int = 0) {
-            deleteCompletions[index](.failure(error))
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deleteCompletions[index](.success(()))
-        }
-        
-        func completeInsertionWithError(_ error: Error, at index: Int = 0) {
-            insertionCompletions[index](.failure(error))
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](.success(()))
-        }
-    }
-    
     private func useCaseRepository() -> (model: Repository, local: LocalRepository) {
         let model = anyRepository()
         let local = LocalRepository(name: model.name, owner: model.owner)
