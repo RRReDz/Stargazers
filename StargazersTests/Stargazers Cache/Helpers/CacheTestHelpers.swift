8ddded9e5e525f2eba1a5c2b39fbf1c7a2a9170e
Riccardo <riki.redz@gmail.com>
2021-09-04 17:57:57 +0200

    Moves use case Stargazer helper creation function to shared file to remove duplications.

--- StargazersTests/Stargazers Cache/Helpers/CacheTestHelpers.swift
+++ StargazersTests/Stargazers Cache/Helpers/CacheTestHelpers.swift
@@ -5,10 +5,34 @@
 //  Created by Riccardo Rossi - Home on 04/09/21.
 //
 
+import Foundation
 import Stargazers
 
 internal func useCaseRepository() -> (model: Repository, local: LocalRepository) {
     let model = anyRepository()
     let local = LocalRepository(name: model.name, owner: model.owner)
     return (model, local)
+}
+
+internal func uniqueStargazer() -> (model: Stargazer, local: LocalStargazer) {
+    let model = Stargazer(
+        id: UUID().uuidString,
+        username: "any",
+        avatarURL: URL(string: "http://any-avatar-url.com")!,
+        detailURL: URL(string: "http://any-detail-url.com")!)
+    
+    let local = LocalStargazer(
+        id: model.id,
+        username: model.username,
+        avatarURL: model.avatarURL,
+        detailURL: model.detailURL)
+    return (model, local)
+}
+
+internal func uniqueStargazers() -> (model: [Stargazer], local: [LocalStargazer]) {
+    let stargazers = [uniqueStargazer(), uniqueStargazer()]
+    return (
+        stargazers.map { $0.model },
+        stargazers.map { $0.local }
+    )
 }
