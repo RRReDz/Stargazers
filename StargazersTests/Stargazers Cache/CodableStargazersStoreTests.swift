7f8ac794a4fb31b2bc55e06a57ea13cab6b67ca5
Riccardo <riki.redz@gmail.com>
2021-10-25 23:52:30 +0200

    Retrieving from empty cache twice delivers same empty result (no-side effects)

--- StargazersTests/Stargazers Cache/CodableStargazersStoreTests.swift
+++ StargazersTests/Stargazers Cache/CodableStargazersStoreTests.swift
@@ -32,5 +32,25 @@ class CodableStargazersStoreTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_retrieve_hasNoSideEffectsOnEmtpyCache() {
+        let sut = CodableStargazersStore()
+        let exp = expectation(description: "Wait for retrieve completion")
+        
+        sut.retrieve(from: LocalRepository(name: "any", owner: "any")) { firstResult in
+            sut.retrieve(from: LocalRepository(name: "any", owner: "any")) { secondResult in
+                switch (firstResult, secondResult) {
+                case let (.success(firstStargazers), .success(secondStargazers)):
+                    XCTAssertEqual(firstStargazers, [])
+                    XCTAssertEqual(secondStargazers, [])
+                default:
+                    XCTFail("Expected both results to be successful and empty, got \(firstResult) and \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
 
 }
