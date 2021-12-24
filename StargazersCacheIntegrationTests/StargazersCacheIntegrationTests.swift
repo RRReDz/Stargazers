fdeb9ede697c2dc54d4782eb27ad2e71e4acec47
Riccardo <riki.redz@gmail.com>
2021-12-24 23:08:00 +0100

    `load` command run in integration delivers no stargazers on empty cache

--- StargazersCacheIntegrationTests/StargazersCacheIntegrationTests.swift
+++ StargazersCacheIntegrationTests/StargazersCacheIntegrationTests.swift
@@ -10,4 +10,23 @@ import Stargazers
 
 class StargazersCacheIntegrationTests: XCTestCase {
     
+    func test_load_deliversNoStargazersOnEmtpyCache() throws {
+        let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(String(describing: StargazersCacheIntegrationTests.self)).store")
+        let store = CodableStargazersStore(storeURL: url)
+        let sut = LocalStargazersLoader(store: store)
+        let repository = Repository(name: "Any repository", owner: "Any owner")
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load(from: repository) { result in
+            switch result {
+            case let .success(stargazers):
+                XCTAssertEqual([], stargazers)
+            default:
+                XCTFail("Expected success with empty stargazers, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
 }
