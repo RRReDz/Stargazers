cef1b5e706a739974ca97a4ce3706020d30f1caa
Riccardo <riki.redz@gmail.com>
2021-12-24 23:06:03 +0100

    Add `StargazersCacheIntegrationTests` testing target to separate cache integration tests (slower) from other testing techniques.

---
+++ StargazersCacheIntegrationTests/StargazersCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  StargazersCacheIntegrationTests.swift
+//  StargazersCacheIntegrationTests
+//
+//  Created by Riccardo Rossi - Home on 24/12/21.
+//
+
+import XCTest
+import Stargazers
+
+class StargazersCacheIntegrationTests: XCTestCase {
+    
+}
