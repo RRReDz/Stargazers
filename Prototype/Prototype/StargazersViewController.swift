e611126b5cd6b3734ca61cd190db2b6996ece244
Riccardo Rossi <riki.redz@gmail.com>
2022-01-16 20:45:41 +0100

    Add StargazersViewController class

---
+++ Prototype/Prototype/StargazersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StargazersViewController.swift
+//  Prototype
+//
+//  Created by Riccardo Rossi - Home on 16/01/22.
+//
+
+import UIKit
+
+class StargazersViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "StargazerCell", for: indexPath)
+    }
+    
+}
