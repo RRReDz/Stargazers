9ece7a757a74f06b80d83e33d420b6c0ff97ba16
Riccardo Rossi <riki.redz@gmail.com>
2022-01-20 00:23:55 +0100

    Add fade-in animation to simulate "asynchronous image loading"

--- Prototype/Prototype/StargazerCell.swift
+++ Prototype/Prototype/StargazerCell.swift
@@ -13,6 +13,26 @@ class StargazerCell: UITableViewCell {
     
     func config(with viewModel: StargazerViewModel) {
         self.userImage.image = UIImage(named: viewModel.imageName)
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0.3,
+            options: [],
+            animations: {
+                self.userImage.alpha = 1
+            })
+        
         self.username.text = viewModel.username
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImage.alpha = 0
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        userImage.alpha = 0
     }
 }
