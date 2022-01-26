2d4e9a2d33e1310da28f88f92a679c4b1a507891
Riccardo Rossi <riki.redz@gmail.com>
2022-01-27 00:06:11 +0100

    Refactor prototype code.

--- Prototype/Prototype/StargazersViewController.swift
+++ Prototype/Prototype/StargazersViewController.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 class StargazersViewController: UITableViewController {
+    private let stargazers = StargazerViewModel.prototypes
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return StargazerViewModel.prototypes.count
+        return stargazers.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let stargazerViewModel = StargazerViewModel.prototypes[indexPath.row]
+        let stargazerViewModel = stargazers[indexPath.row]
         
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "StargazerCellWithImage",
